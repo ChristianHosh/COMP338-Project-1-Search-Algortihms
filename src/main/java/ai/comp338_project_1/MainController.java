2c71a12beef250c54cad1f5ba63220748331a1c9
ChristianHosh <mctibhosh@gmail.com>
2023-06-01 01:14:48 +0300

    Initial commit

---
+++ src/main/java/ai/comp338_project_1/MainController.java
@@ -0,0 +1,14 @@
+package ai.comp338_project_1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
